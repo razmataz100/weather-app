19966f29aa2307fba3434bce27a35c31477269ba
unknown <rasmussandsjo@gmail.com>
2024-01-17 15:14:46 +0100

    added components and a structure for css

---
+++ src/Components/Forecast/Forecast.jsx
@@ -0,0 +1,22 @@
+import './Forecast.css'
+
+const Forecast = () => {
+
+
+    return(
+        <>
+        <div className="forecast-container">
+        <h4>Forecast</h4>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>    
+        
+        </>
+
+    );
+
+}
+export default Forecast;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+
+const Header = () => {
+
+    return(
+        <>
+        <div className="top-bar-container">
+        <h1 id="website-name">Clear Sky</h1>
+        </div>
+        </>
+    );
+}
+export default Header;
